5e57b8f53b74b2eba27e759c035f936beb8cbfde
parmita16 <parmitarajthala2021@gmail.com>
2026-07-04 22:09:21 +0545

    useNavigate()

--- src/pages/Character.jsx
+++ src/pages/Character.jsx
@@ -1,4 +1,17 @@
+import { useParams, useNavigate } from "react-router-dom";
 function Character() {
-  return <h1>Character Page</h1>;
+  const { id } = useParams();
+  const navigate = useNavigate();
+  return (
+    <>
+      <h1>{id}</h1>
+      <button onClick={() => navigate("/")}>
+        Go Home
+      </button>
+      <button onClick={() => navigate(-1)}>
+        Go Back
+      </button>
+    </>
+  );
 }
 export default Character;
