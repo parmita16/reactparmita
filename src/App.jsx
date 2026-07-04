ad92d1ba469ce2db6222a67b71b94f8c16390e50
parmita16 <parmitarajthala2021@gmail.com>
2026-07-04 22:18:30 +0545

    page not found

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,21 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Character from "./pages/Character";
+import NotFound from "./pages/NotFound";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Home Page */}
         <Route path="/" element={<Home />} />
+        {/* About Page */}
         <Route path="/about" element={<About />} />
+        {/* Contact Page */}
         <Route path="/contact" element={<Contact />} />
+        {/* Dynamic Route */}
         <Route path="/character/:id" element={<Character />} />
+        {/* 404 Page - Keep this LAST */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
