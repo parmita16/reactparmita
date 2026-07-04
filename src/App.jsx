ce3f65197d7a477baba71deded657a6576e70b6f
parmita16 <parmitarajthala2021@gmail.com>
2026-07-04 22:00:03 +0545

    Character Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Character from "./pages/Character";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/character/:id" element={<Character />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Character.jsx
@@ -0,0 +1,4 @@
+function Character() {
+  return <h1>Character Page</h1>;
+}
+export default Character;
